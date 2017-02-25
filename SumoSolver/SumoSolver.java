a1b20425f4af6ab225011ebd2a1c2eb9ab1a6edc
Ethan Arns <narkro555@gmail.com>
2017-02-25 14:08:10 -0800

    prepared graphical help

--- SumoSolver/SumoSolver.java
+++ SumoSolver/SumoSolver.java
@@ -1,13 +1,14 @@
 // http://pdorin.cs.lmu.edu/282.stuff/SumoSolver.html
 class SumoSolver {
     // Main method
-    // Takes in an array of numbers as Strings to build into list of food and money
+    // Takes in an array of numbers as Strings from execution on command line
+    // Checks arguments, converts to ints, then hands off to instantiated Solver
     public static void main(String args[]) {
-        System.out.println("Hello world!");
         if( ((args.length - 1) % 2 != 0) || args.length < 3 ) {
             System.out.println("ERROR: Invalid number of arguments.");
             return;
         }
+        // foodArray is args[], converted to ints
         int[] foodArray = new int[args.length];
         int testNum = -1;
         for(int i = 0; i < foodArray.length; i++) {
@@ -16,6 +17,10 @@ class SumoSolver {
             }
             catch (NumberFormatException e) {
                 System.out.println("ERROR: Argument not a number!");
+                return;
+            }
+            if(testNum < 0) {
+                System.out.println("ERROR: No negative numbers.");
                 return;
             }
             foodArray[i] = testNum;
@@ -36,6 +41,42 @@ class SumoSolver {
             System.out.println(f);
         }
         System.out.println("Money: " + money);
+        //First is 3 combos of food, second is 0 - money amount, last is tuple size
+        int[][][] memo = new int[foodList.length][money + 1][foodList.length];
+        printList(memo);
+
+        // How it works
+        /*
+        Memoization vastly increases speed by saving previous sub-problems
+        and then going back and checking it later to see if its the most
+        optimal. So with this, we have a variety of food the sumo wrestler
+        can get 1 or 0 of. This is a variation of the change problem, which
+        you need a 2D array for. However you can only use 1 at most, and are trying to maximize profit
+        */
+    }
+
+    public void printList(int list[][][]) {
+        String len = "";
+        for(int m = 0; m < list[0].length; m++) {
+            System.out.print(" " + m);
+            for(int n = 0; n < 7 - ("" + m).length(); n++) {
+                System.out.print(" ");
+            }
+        }
+        System.out.println();
+        for(int i = 0; i < list.length; i++) {
+            for(int j = 0; j < list[0].length; j++) {
+                System.out.print("[");
+                for(int k = 0; k < list[0][0].length; k++) {
+                    System.out.print("" + list[i][j][k]);
+                    if(k != list[0][0].length - 1) {
+                        System.out.print(",");
+                    }
+                }
+                System.out.print("] ");
+            }
+            System.out.println();
+        }
     }
 
     // Subclass for more organized handling of items
@@ -49,7 +90,8 @@ class SumoSolver {
         }
 
         public String toString() {
-            return "Food(cost: " + cost + ", pounds: " + pounds + ")";
+            //return "Food(cost: " + cost + ", pounds: " + pounds + ")";
+            return("[" + cost + "," + pounds + "]");
         }
     }
 
