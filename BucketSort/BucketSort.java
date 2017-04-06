1b13de63057e720c4369518e99fd6e7af00a16cb
Ethan Arns <ethanarns@Ethans-Shiny-Gameless-Brick.local>
2017-04-06 11:25:32 -0700

    Two base uploads

---
+++ BucketSort/BucketSort.java
@@ -0,0 +1,5 @@
+public class BucketSort {
+  public static void main(String args[]) {
+    System.out.println("Hello World!");
+  }
+}
---
+++ CrownAndAnchor/CrownAndAnchor.java
@@ -0,0 +1,5 @@
+public class CrownAndAnchor {
+  public static void main(String args[]) {
+    System.out.println("Hello world!");
+  }
+}
